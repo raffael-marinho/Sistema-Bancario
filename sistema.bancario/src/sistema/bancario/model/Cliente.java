46e1dd201b65323aa2d1a42bf0122e152a8d933d
raffael-marinho <silvamarinho10@gmail.com>
2024-12-10 22:30:32 -0300

    adiciona constructor de Cliente

--- sistema.bancario/src/sistema/bancario/model/Cliente.java
+++ sistema.bancario/src/sistema/bancario/model/Cliente.java
@@ -1,5 +1,17 @@
 package sistema.bancario.model;
 
-public class Cliente {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Cliente {
+	private String nome;
+	private String cpf;
+	private List<Conta> contas;
+
+	public Cliente(String nome, String cpf) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.contas = new ArrayList<>();
+		
+	}
 }
