ef3966dc93268bcdca7f271bad1b77318592cc8c
raffael-marinho <silvamarinho10@gmail.com>
2024-12-10 21:14:42 -0300

    adiciona metodos basicos

--- sistema.bancario/src/sistema/bancario/model/Conta.java
+++ sistema.bancario/src/sistema/bancario/model/Conta.java
@@ -1,10 +1,28 @@
 package sistema.bancario.model;
 
 public class Conta {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	private String numeroConta;
+	private double saldo;
+	
+	public Conta(String numeroConta, double saldoInicial) {
+		this.numeroConta = numeroConta;
+		this.saldo = saldoInicial;
 	}
-
+	
+	public String getNumeroConta() {
+		return numeroConta;
+	}
+	
+	public void setNumeroConta(String numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+	
+	public double getSaldo() {
+		return saldo;
+	}
+	
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
 }
