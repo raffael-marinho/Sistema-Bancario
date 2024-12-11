6daab32798ea27fbbf76e5514bf4c0ff6af068c2
raffael-marinho <silvamarinho10@gmail.com>
2024-12-10 23:32:42 -0300

    adiciona os metodos de ClienteService

--- sistema.bancario/src/sistema/bancario/service/ClienteService.java
+++ sistema.bancario/src/sistema/bancario/service/ClienteService.java
@@ -1,5 +1,34 @@
 package sistema.bancario.service;
 
-public class ClienteService {
+import java.util.ArrayList;
+import java.util.List;
 
+import sistema.bancario.model.Cliente;
+import sistema.bancario.model.Conta;
+
+public class ClienteService {
+	private List<Cliente> clientes = new ArrayList<>();
+	
+	public void adicionaClientes(Cliente cliente) {
+		clientes.add(cliente);
+        System.out.println("Cliente " + cliente.getNome() + " adicionado com sucesso!");
+	}
+	
+	public void listarClientes() {
+        for (Cliente cliente : clientes) {
+            System.out.println(cliente);
+        }
+    }
+	
+	public void adicionarConta(Cliente cliente, Conta conta) {
+        cliente.adicionarConta(conta);
+    }
+	
+	public void removerConta(Cliente cliente, Conta conta) {
+        cliente.removerConta(conta);
+    }
+	
+	public List<Cliente> getClientes() {
+        return clientes;
+    }
 }
