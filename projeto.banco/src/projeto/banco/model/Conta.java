2cd14204d81b598751a0581ec6a72f0cec673de2
raffael-marinho <silvamarinho10@gmail.com>
2024-12-17 20:58:31 -0300

    cria o constructor de Conta

--- projeto.banco/src/projeto/banco/model/Conta.java
+++ projeto.banco/src/projeto/banco/model/Conta.java
@@ -1,5 +1,23 @@
 package projeto.banco.model;
 
+import java.time.LocalDateTime;
+
 public class Conta {
+	private Integer numeroDaConta;
+	private float saldo;
+	private LocalDateTime dataAbertura;
+	private boolean status;
+	
+	public Conta() {
+		
+	}
+	
+	public Conta(Integer numero) {
+		this.numeroDaConta = numeroDaConta;
+		this.saldo = 0f;
+		this.dataAbertura = LocalDateTime.now();
+		this.status = true;
+	}
+	
 
 }
