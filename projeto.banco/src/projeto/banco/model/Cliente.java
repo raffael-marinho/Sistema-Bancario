192c7878dc226711b53fe6921c2d8c4092855dcc
raffael-marinho <silvamarinho10@gmail.com>
2024-12-18 00:12:48 -0300

    cria os metodos getters setters hascode e o toString

--- projeto.banco/src/projeto/banco/model/Cliente.java
+++ projeto.banco/src/projeto/banco/model/Cliente.java
@@ -1,6 +1,7 @@
 package projeto.banco.model;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Cliente {
 	private String cpf;
@@ -46,5 +47,58 @@ public class Cliente {
 		return null;
 	}
 	
-	
+	public void atualizarConta(Conta contaAntiga) {
+		if(contas.contains(contaAntiga)) {
+			int index = contas.indexOf(contaAntiga);
+			contas.set(index, contaAntiga);
+			System.out.println("Conta atualizada com sucesso");
+		}else
+			System.out.println("Conta n�o localizada");
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public ArrayList<Conta> getContas() {
+		return contas;
+	}
+
+	public void setContas(ArrayList<Conta> contas) {
+		this.contas = contas;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(cpf, other.cpf);
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [cpf=" + cpf + ", nome=" + nome + ", contas=" + contas + "]";
+	}
 }
