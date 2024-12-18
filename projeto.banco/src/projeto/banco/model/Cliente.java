5d26a42d8a16de650a1e881bbe3f081f90f055c2
raffael-marinho <silvamarinho10@gmail.com>
2024-12-18 00:11:17 -0300

    adiciona os metodos: localizarConta e atualizarConta

--- projeto.banco/src/projeto/banco/model/Cliente.java
+++ projeto.banco/src/projeto/banco/model/Cliente.java
@@ -18,21 +18,32 @@ public class Cliente {
 		contas = new ArrayList<>();
 	}
 	
-	public void adicionarConta(Conta outraConta) {
-		if(contas.contains(outraConta)) {
+	public void adicionarConta(Conta novaConta) {
+		if(contas.contains(novaConta)) {
 			System.out.println("Conta já cadastrada");
 		} else {
 			System.out.println("Conta cadastrada com sucesso");
 		}
 	}
 	
-	public void removerConta(Conta outraConta) {
-		if(contas.contains(outraConta)) {
-			contas.remove(outraConta);
+	public void removerConta(Conta velhaConta) {
+		if(contas.contains(velhaConta)) {
+			contas.remove(velhaConta);
 			System.out.println("Conta removida com sucesso!");
 		} else {
 			System.out.println("Conta nao localizada");
 		}
+	}
+	
+	public Conta localizarContaPorNumero(Integer numero) {
+		Conta temp = new Conta(numero);
+		
+		if(contas.contains(temp)) {
+			int index = contas.indexOf(temp);
+			temp = contas.get(index);
+			return temp;
+		}
+		return null;
 	}
 	
 	
