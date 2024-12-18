52fb9b8aeb66ceff25a9d3b9570ebeb41b07f461
raffael-marinho <silvamarinho10@gmail.com>
2024-12-18 13:34:55 -0300

    pega os conceito do programa feito pelo professor em app

--- projeto.banco/src/projeto/banco/app/Programa.java
+++ projeto.banco/src/projeto/banco/app/Programa.java
@@ -1,5 +1,63 @@
 package projeto.banco.app;
 
+import java.util.Scanner;
+
+import bancario.projeto.persistencia.PersistenciaCliente;
+import projeto.banco.model.Cliente;
+
 public class Programa {
+public static void main(String[] args) {
+		
+		PersistenciaCliente p = new PersistenciaCliente();
+		
+		Scanner sc = new Scanner(System.in);
+		
+		boolean sair = true;
+		int opcao = 0;
+		System.out.println("Bem vindo ao sistema banc�rio!!");
+		
+		while(sair) {
+			System.out.println("\n\n\nDigite a opcao desejada:\n"
+					+ "\n1 - Cadastro de cliente;\n"
+					+ "2 - Remover cliente\n"
+					+ "3 - Opcoes de cliente\n"
+					+ "4 - Para sair\n\n\n");
+			opcao = sc.nextInt();
+			
+			switch (opcao) {
+			case 1: {
+				String cpf;
+				String nome;
+				System.out.println("Insira o seu CPF: ");
+				cpf = sc.next();
+				System.out.println("Insira o seu nome: ");
+				nome = sc.next();
+				p.adicionarCliente(new Cliente(cpf, nome));
+				break;
+			}
+			case 2: {
+				Cliente temp;
+				String cpf;
+				System.out.println("Insira o seu CPF: ");
+				cpf = sc.next();
+				temp = new Cliente();
+				temp.setCpf(cpf);
+				p.removerCliente(temp);
+				break;
+			}
+			case 3: {
+				System.out.println("Opcao 3 selecionada");
+				break;
+			}
+			case 4: {
+				sair = false;
+				System.out.println("Opcao 4 selecionada");
+				break;
+			}
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + opcao);
+			}
+		}
+	}
 
 }
