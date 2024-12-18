9aa87e8968abf03fed35572aa8f68c0cd66a91d5
raffael-marinho <silvamarinho10@gmail.com>
2024-12-17 21:01:22 -0300

    cria os metodos getters setters hascode e o toString

--- projeto.banco/src/projeto/banco/model/Conta.java
+++ projeto.banco/src/projeto/banco/model/Conta.java
@@ -1,6 +1,7 @@
 package projeto.banco.model;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Conta {
 	private Integer numeroDaConta;
@@ -18,6 +19,60 @@ public class Conta {
 		this.dataAbertura = LocalDateTime.now();
 		this.status = true;
 	}
-	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(numeroDaConta);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Conta other = (Conta) obj;
+		return Objects.equals(numeroDaConta, other.numeroDaConta);
+	}
+
+	public Integer getNumeroDaConta() {
+		return numeroDaConta;
+	}
+
+	public void setNumeroDaConta(Integer numeroDaConta) {
+		this.numeroDaConta = numeroDaConta;
+	}
+
+	public float getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+	}
+
+	public LocalDateTime getDataAbertura() {
+		return dataAbertura;
+	}
+
+	public void setDataAbertura(LocalDateTime dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Conta [numeroDaConta=" + numeroDaConta + ", saldo=" + saldo + ", dataAbertura=" + dataAbertura
+				+ ", status=" + status + "]";
+	}
 
 }
